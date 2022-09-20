c6d21f5d1ed35a18febe694441d33b812790ff0e
hanuuuul <hnauul.h@gmail.com>
2022-09-20 22:55:16 +0900

    update

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
+const startBtn = document.getElementById('startBtn');
+const stageNum = document.getElementById('stageNum');
 let gameStage = 1;
 let pcSelect = [];
+let isPcSelectDone = false;
 
+//게임 시작                     
+function gameStart(){
+  startBtn.addEventListener('click', function(e){
+    e.target.style.display = 'none';
+    stageNum.style.display = 'block';
+    stageStart().then(userSelect);
+  });
+}
+                            
 //스테이지 start
 function stageStart(){
     //게임시작 카운트
     let cnt = 3;
     let startTimer = setInterval(function(){
-        if(cnt > 0){
-            document.getElementById('count').innerHTML = cnt;
-            cnt -= 1;
-        }else{
-            document.getElementById('count').innerHTML = '';
-            clearInterval(startTimer);
-        }
-    }, 1000);
+      if(cnt > 0){
+        document.getElementById('count').innerHTML = cnt;
+        cnt -= 1;
+      }else{
+        document.getElementById('count').innerHTML = '';
+        clearInterval(startTimer);
+      }
+    }, 500);
     //문제 출제 - pcSelect
+  return new Promise(function(resolve){
     setTimeout(function(){
-        for(let i=0; i<gameStage; i++){
-            pcSelect.push(Math.floor(Math.random() * 9) + 1);
-            setTimeout(function(){
-                document.getElementById(pcSelect[i]).className = 'btnArea pcSelect';
-                setTimeout(function(){
-                    document.getElementById(pcSelect[i]).classList.remove("pcSelect");
-                },500);
-            }, 1000*i);
-        }
-    }, 4000);
+      for(let i=0; i<gameStage; i++){
+        pcSelect.push(Math.floor(Math.random() * 9) + 1);
+        setTimeout(function(){
+          document.getElementById(pcSelect[i]).className = 'btnArea pcSelect';
+          setTimeout(function(){
+            document.getElementById(pcSelect[i]).classList.remove("pcSelect");
+            if(i == gameStage - 1){
+              isPcSelectDone = false;
+            }
+          },300);
+        }, 600*i);
+      }
+      resolve();
+    }, 2400);
+    //게임 스테이지
+    setTimeout(function(){
+        stageNum.innerHTML = 'STAGE ' + gameStage;
+    }, 400);
+  });
 }
 
 //문제 풀이 userSelect
@@ -37,11 +59,12 @@ function userSelect(){
     //유저가 영역 클릭하면 할일
     for(let i=0; i<9; i++){
         btnArea[i].addEventListener('click', function(e){
+            if(isPcSelectDone == true){return false};
             //클릭영역 깜빡이고, userClickId에 id값을 추가
             e.currentTarget.className = 'btnArea userSelect';
             setTimeout(function(){
                 btnArea[i].classList.remove("userSelect");
-            }, 500);
+            }, 300);
             userClickId = e.currentTarget.getAttribute('id');
 
             //정답 체크
@@ -56,6 +79,7 @@ function userSelect(){
                 console.log('continue');
                 if(userClickCnt == gameStage){
                     //스테이지 성공
+                    isPcSelectDone = true;
                     console.log('stageClear');
                     gameStage += 1;
                     pcSelect = [];
@@ -67,11 +91,6 @@ function userSelect(){
         }, false);
     }
 }
-userSelect();
-
-//게임 시작
-const startBtn = document.getElementById('startBtn');
-startBtn.addEventListener('click',function(e){
-    stageStart();
-    e.target.style.display = 'none';
-});
+  
+  
+gameStart();
